83d9462913877b650920331c18ebd6fed0918ce2
marcoIDS <163167@ids.upchiapas.edu.mx>
2020-01-07 12:27:58 -0600

    add funtion of map

--- src/app/core/map/map.module.ts
+++ src/app/core/map/map.module.ts
@@ -2,20 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MapComponent } from './map.component';
-import { RealtimeMapComponent } from './realtime-map/realtime-map.component';
+import { RealtimeMapComponent, DialogOverviewModalDialogComponent } from './realtime-map/realtime-map.component';
 import { RouterModule } from '@angular/router';
 import { MapRoutingModule } from './map-routing.module';
+import { MaterialModule } from 'src/app/material/material.module';
 
 @NgModule({
   declarations: [
       MapComponent,
-      RealtimeMapComponent
+      RealtimeMapComponent,
+      DialogOverviewModalDialogComponent,
     ],
   imports: [
     CommonModule,
     FormsModule,
     MapRoutingModule,
     ReactiveFormsModule,
+    MaterialModule,
+  ],
+  entryComponents: [
+    DialogOverviewModalDialogComponent,
   ]
 })
 export class MapModule { }
